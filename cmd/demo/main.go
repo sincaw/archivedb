45fd95d8492c02a95cab5e784a8f8d5e31368a08
jialei <jialeicui@126.com>
2022-04-26 23:13:41 +0800

    fill demo data for serving test

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -2,20 +2,61 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/jialeicui/archivedb/pkg"
 )
 
+func getImage(url string) ([]byte, error) {
+	if url == "" {
+		url = "https://picsum.photos/id/0/200/300"
+	}
+	response, e := http.Get(url)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer response.Body.Close()
+	return ioutil.ReadAll(response.Body)
+}
+
 func main() {
+	c, err := ioutil.ReadFile("demo.json")
+	if err != nil {
+		panic(err)
+	}
+
+	item := pkg.Item{}
+	err = bson.UnmarshalExtJSON(c, true, &item)
+	if err != nil {
+		panic(err)
+	}
+
+	os.RemoveAll("data")
 	d, err := pkg.New("data")
 	if err != nil {
 		panic(err)
 	}
 	defer d.Close()
 
-	err = d.Put(&pkg.Item{"A": "B", "C": pkg.Item{"D": "E"}})
+	binaryContent, err := getImage("")
 	if err != nil {
 		panic(err)
+	}
+
+	for _, i := range item["data"].(bson.A) {
+		it := i.(bson.M)
+		err = d.Put(&it,
+			pkg.WithResources(pkg.Resources{"img": binaryContent}),
+			pkg.WithKey([]byte(it["idstr"].(string))),
+		)
+		if err != nil {
+			panic(err)
+		}
 	}
 	it, err := d.Find(pkg.Query{})
 	if err != nil {
