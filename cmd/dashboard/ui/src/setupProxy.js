1cda73ed5a05882a6794ab1faa124417911453c3
jialei <jialeicui@126.com>
2022-05-16 23:48:28 +0800

    support url params

--- cmd/dashboard/ui/src/setupProxy.js
+++ cmd/dashboard/ui/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = (app) => {
-  app.use(proxy.createProxyMiddleware(['/list', '/resource'], {
+  app.use(proxy.createProxyMiddleware(['/api'], {
     target: process.env.PROXY || 'http://127.0.0.1:8000', changeOrigin: true, secure: false
   }))
 }
